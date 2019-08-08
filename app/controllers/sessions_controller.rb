77f9869b70b311925935de8a1716c1d45cca4885
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 04:01:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
-  def new
-  end
+    def new
+    end
 
-  def create
-    if name_from_params
-      session[:name] = name_from_params
-      redirect_to root_path
-    else
+
+    def create
+      if params[:name].present?
+          session[:name] = params[:name]
+        redirect_to root_path
+      else
+        redirect_to login_path
+      end
+    end
+
+    def destroy
+      session.delete :name
       redirect_to login_path
     end
-  end
 
-  def destroy
-    session.delete :name
-    redirect_to login_path
-  end
 end
