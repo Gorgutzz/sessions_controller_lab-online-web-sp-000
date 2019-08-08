f5b72347f91437d04b72cc6121063cecc9a86180
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-08 04:05:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         redirect_to '/login'
       else
         session[:name] = params[:name]
-        redirect_to login_path
+        redirect_to "/"
       end
     end
 
